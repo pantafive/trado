d563859ac52f5d62a40a9a99a09de2518d66f10e
Grigory Bakunov <thebobuk@ya.ru>
2022-02-03 16:43:08 +0200

    add docs. add `restart`

--- README.md
+++ README.md
@@ -1,12 +1,15 @@
 # trado
 Dumb and dirty docker-compose files generator for traefik exposing
 
-All services described in one services.yml file and converted to more or less docker-compose compatible blocks. 
+All services described in one services.toml file and converted to more or less docker-compose compatible blocks. 
 Top-level keys is services name, images, environment, volumes, ports is mirrored to docker-compose.
-Important part is `url` key, which is used to generate traefik labels.
-`url` signature is:
+
+## Services exposure to traefik
+
+The most complicated part is `public` key, which is used to generate traefik labels.
+`public` signature is:
 ```yaml
-url: <host>[/<path>][@port]
+public: <host>[/<path>][@port]
 ```
 
 - `host` directly used as hostname in traefik labels. TLS with letsencrypt is enabled by default.
@@ -14,32 +17,46 @@ url: <host>[/<path>][@port]
    Be careful, `host` without path must be configured in a diffrenent service at least once.
 - `port`: it's a hint for traefik to find a proper port for proxying
 
-Notice, that default `networks` for all services with `url` defined is `traefik`.
+Notice, that default `networks` for all services with `public` defined is `traefik`.
 
-```yaml
-gitea:
-  image: gitea/gitea:latest
-  append:
-    environment:
-      - USER_UID=1000
-      - USER_GID=1000
-    volumes:
-      - ./data:/data
-    ports:
-      - "3000:3000"
-      - "2222:22"
-  url: git.rubedo.cloud @ 3000
-  expose: 3000
-asdf:
-  image: containous/whoami
-  url: asdf.rubedo.cloud
-whoami2:
-  image: containous/whoami
-  url: git.rubedo.cloud /test
-  append:
-    restart: unless-stopped
-    labels:
-      - "testlabel=true"
-blah:
-  image: containous/whoami # not exposed to traefik at all
+## Options
+
+- `image`: docker image name
+- `envs`: list of environment variables
+  - `doppler`: extract list of variables from [doppler](doppler.com/)
+- `volumes`: list of volumes
+- `ports`: list of ports
+- `restart`: restart policy, default is `always`
+- `networks`: list of networks. Default is `traefik` for every service with `public` defined but can be extended.
+- `labels`: list of labels. For every `public` service it's filled with traefik-specific labels.
+  - `watchtower`: add label to enable and disable autoupdate of service with [watchtower](https://github.com/containrrr/watchtower)
+## Example
+
+```toml
+[gitea]
+image = "gitea/gitea:latest"
+public = "git.rubedo.cloud @ 3000"
+doppler = true
+wathctower = true
+volumes = ["./data:/data"]
+
+ports = ["3000:3000", "2222:22"]
+    [gitea.envs]
+    USER_UID = 1000
+    USER_GID = 1000
+    TEST = "test"
+
+[asdf]
+image = "containous/whoami"
+public = "asdf.rubedo.cloud"
+restart = "unless-stopped"
+
+[whoami2]
+image = "containous/whoami"
+public = "git.rubedo.cloud /test"
+restart = "unless-stopped"
+labels = "testlabel=true"
+
+[blah]
+image = "containous/whoami" # not exposed to traefik at all
 ```
--- trado.py
+++ trado.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python3
 import pathlib
-import yaml
+import yaml, tomli
 import sys
 import subprocess
 from dataclasses import dataclass, field, asdict
 
-
-SOURCE = "services.yml"
-OUTPUT = "docker-compose.yml"
+DEFAULT_SOURCE = "services.toml"
+DEFAULT_OUTPUT = "docker-compose.yml"
 
 
 class TradoException(Exception):
@@ -31,53 +30,93 @@ class DockerCompose:
 
 @dataclass
 class Trado:
-    name: str = ""
-    container_name: str = ""
+    name: str
     image: str = ""
+
     url_host: str = ""
     url_prefix: str = ""
     url_expose: int = 0
+
     labels: list[str] = field(default_factory=list)
+    envs: list = field(default_factory=list)
+    volumes: list = field(default_factory=list)
+    ports: list = field(default_factory=list)
+    restart: str = "always"
+
     doppler: bool = False
-    append: dict = field(default_factory=dict)
+    watchtower: bool = False
+    networks: list = field(default_factory=list)
 
     @staticmethod
     def from_dict(name: str, tr: dict) -> "Trado":
         gen = Trado(name=name)
         gen.image = tr.get("image", "")
-        gen.url_host = tr.get("url", "")
-        if gen.url_host:
-            if '@' in gen.url_host:
-                gen.url_host, gen.url_expose = [x.strip() for x in gen.url_host.rsplit("@",1)]
-            if '/' in gen.url_host:
-                gen.url_host, gen.url_prefix = [x.strip() for x in gen.url_host.split("/", 1)]
-                gen.url_prefix = "/" + gen.url_prefix
-        gen.container_name = tr.get("container_name", "")
+        gen.get_url_host_splitted(tr)
+
+        gen.labels = Trado.get_complex_by_type(tr, "labels")
+        gen.envs = Trado.get_complex_by_type(tr, "envs")
+        gen.volumes = Trado.get_complex_by_type(tr, "volumes")
+        gen.ports = Trado.get_complex_by_type(tr, "ports")
+        gen.networks = Trado.get_complex_by_type(tr, "networks")
+
+        gen.restart = tr.get("restart", "always").lower()
+        if gen.restart not in ["always", "on-failure", "unless-stopped", "no"]:
+            raise TradoException(f"{gen.restart } is not a valid value for `restart` option")
+
         gen.doppler = tr.get("doppler", False)
-        gen.append = tr.get("append", {})
+        gen.watchtower = tr.get("watchtower", False)
         return gen
 
+    def get_url_host_splitted(self, tr: dict):
+        self.url_host = tr.get("public", "")
+        if self.url_host:
+            if '@' in self.url_host:
+                self.url_host, self.url_expose = [x.strip() for x in self.url_host.rsplit("@", 1)]
+            if '/' in self.url_host:
+                self.url_host, self.url_prefix = [x.strip() for x in self.url_host.split("/", 1)]
+                self.url_prefix = "/" + self.url_prefix
+
+    @staticmethod
+    def get_complex_by_type(tr: dict, element: str) -> list:
+        item = tr.get(element, [])
+        if type(item) is list:
+            return item
+        elif type(item) is dict:
+            return [f"{k}={v}" for k, v in item.items()]
+        elif type(item) is str:
+            return [item]
+        else:
+            raise TradoException(f"`{element}` is not a list or map, key confilict")
+
     def to_dict(self) -> dict:
-        res = {}
         dt = asdict(self)
         del dt["name"]
 
         if self.url_host:
             self.add_network_labels(dt)
+        else:
+            dt["labels"] = []
         if self.doppler:
             self.add_doppler(dt)
-        if self.append:
-            self.add_appendex(dt)
-        for k, v in dt.items():
-            if v:
-                res[k] = v
+        if not self.watchtower:
+            self.add_watchtower_disabled(dt)
+
+        if "envs" in dt:
+            if "environment" not in dt:
+                dt["environment"] = self.envs
+            else:
+                dt["environment"].extend(self.envs)
+            del dt["envs"]
         for k in ["url_host", "url_prefix", "url_expose"]:
-            if k in res:
-                del res[k]
-        return res
+            if k in dt:
+                del dt[k]
+        return {k: v for k, v in dt.items() if v}
 
     def add_network_labels(self, dt):
-        dt["networks"] = ["traefik"]
+        if "networks" in dt:
+            dt["networks"].append("traefik")
+        else:
+            dt["networks"] = ["traefik"]
         labels: list[str] = ["enable=true"]
         if self.url_expose:
             labels.append(f"http.services.{self.name}.loadbalancer.server.port={self.url_expose}")
@@ -105,48 +144,41 @@ class Trado:
             raise TradoException("doppler secrets --json failed")
         del dt["doppler"]
 
-    def add_appendex(self, dt):
-        for k, v in self.append.items():
-            if k in dt:
-                if type(dt[k]) is list:
-                    dt[k] = dt[k] + v
-                elif type(dt[k]) is dict:
-                    dt[k].update(v)
-                else:
-                    raise TradoException(f"{k} is not a list or map, key confilict")
-            else:
-                dt[k] = v
-        del dt["append"]
+    def add_watchtower_disabled(self, dt):
+        dt["labels"].append(f"com.centurylinklabs.watchtower.enable=false")
+        del dt["watchtower"]
+
+
+def make_dc_from_toml(srcpath):
+    dc = DockerCompose()
+    traefik_needed = False
+    data = tomli.loads(srcpath.read_text())
+    for service in data.keys():
+        tr = Trado.from_dict(service, data[service])
+        try:
+            dc.services[service] = tr.to_dict()
+        except TradoException as e:
+            sys.stderr.write(f"Services: {service}: {e}\n")
+            sys.exit(-5)
+        if net := dc.services[service].get("networks", None):
+            if "traefik" in net:
+                traefik_needed = True
+        if not traefik_needed:
+            dc.networks = {}
+    return dc
 
 
 def main():
-    srcpath = pathlib.Path(SOURCE)
+    output = " ".join(sys.argv[1:]) if sys.argv[1:] else DEFAULT_OUTPUT
+    srcpath = pathlib.Path(DEFAULT_SOURCE)
     if not srcpath.exists():
-        sys.stderr.write(f"Services: {SOURCE} not found\n")
+        sys.stderr.write(f"Services: {DEFAULT_SOURCE} not found\n")
         sys.exit(-1)
-    output = OUTPUT
-    if sys.argv[1:]:
-        output = sys.argv[1]
-    dstpath = pathlib.Path(output) if output != '--' else '--'
-    if dstpath.exists() and dstpath.stat().st_mtime > srcpath.stat().st_mtime:
-        sys.stderr.write(f"Services: {output} is same or newer than {SOURCE}, stop.\n")
+    dstpath = pathlib.Path(output) if output != '-' else '-'
+    if dstpath != '-' and dstpath.exists() and dstpath.stat().st_mtime > srcpath.stat().st_mtime:
+        sys.stderr.write(f"Services: {output} is same or newer than {DEFAULT_SOURCE}, stop.\n")
         sys.exit(0)
-    dc = DockerCompose()
-    traefik_needed = False
-    with open(srcpath, "r") as f:
-        data = yaml.load(f, Loader=yaml.FullLoader)
-        for service in data.keys():
-            tr = Trado.from_dict(service, data[service])
-            try:
-                dc.services[service] = tr.to_dict()
-            except TradoException as e:
-                sys.stderr.write(f"Services: {service}: {e}\n")
-                sys.exit(-5)
-            if net := dc.services[service].get("networks", None):
-                if "traefik" in net:
-                    traefik_needed = True
-        if not traefik_needed:
-            dc.networks = {}
+    dc = make_dc_from_toml(srcpath)
     if dstpath == '-':
         print(dc.to_yaml())
     else:
